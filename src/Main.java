3dab77820bc22d3cdc57ae6b74526f7327f88553
Maria Negretskaya <marian.93@mail.ru>
2022-04-29 22:18:14 +0300

    Code reformat

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 80;
+        int mile = 20;
+        System.out.println("Количество начисляемых миль за один билет");
+        System.out.println((ticket / mile) + " миль ");
+    }
+}
